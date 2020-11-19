1e4393db99c93e2f6e09c2cede1b3224d0aaa670
RAMAMURTHI <ramamurthi@Administrators-MacBook-Pro-135.local>
2020-11-20 02:50:23 +0530

    Small Changes

--- EasyOS.py
+++ EasyOS.py
@@ -6,6 +6,7 @@ import os
 from os.path import join
 import shutil
 
+
 class GitObject:
     def __init__(self, PATH_TO_CONFIG=None, REPO=None, dest=""):
         print("Starting GitObject Initialized")
@@ -16,7 +17,7 @@ class GitObject:
         config.read(PATH_TO_CONFIG)
         self.user = config["GitHub"]["USERNAME"]
         self.token = config["GitHub"]["TOKEN"]
-        self.curr_dir = self.parent_dir
+        self.curr_dir = os.getcwd()
         print("GitObject Initialized Completed")
         self.printing_values()
 
@@ -26,56 +27,56 @@ class GitObject:
         print("User Accessd : ", self.user)
         print("Token Accessd : ", self.token)
         print("Destination_Path : ", self.dest)
-        print("Parent Wk_dir :",self.parent_dir)
-        print("Current Working Dir :",self.curr_dir)
+        print("Parent Wk_dir :", self.parent_dir)
+        print("Current Working Dir :", self.curr_dir)
         print("*" + ("-" * 100) + "*")
 
     def run(*args):
         return subprocess.check_call(["git"] + list(args))
-    
-    def abs_path(self , path):
-        return join(self.parent_dir , path )
+
+    def abs_path(self, path):
+        return join(self.parent_dir, path)
 
     def list_all_dir(self):
         """ See the list of all dir tree"""
         curr_wk_dir = f"{str(os.getcwd())}"
         print(
-            "listing all files and directories under working directory :", curr_wk_dir
-        ," -- ")
+            "listing all files and directories under working directory :",
+            curr_wk_dir,
+            " -- ",
+        )
         print(os.listdir())
 
-    def mk_dir(self,dir_name):
+    def mk_dir(self, dir_name):
         """ See the list of all dir tree"""
         curr_wk_dir = f"{str(os.getcwd())}"
-        dir_path = join(curr_wk_dir,dir_name)
-        print(
-            "Creating a new directory in the following path :", dir_path
-        ," -- ")
-        
-        try: 
-            os.mkdir(dir_path) 
-        except OSError as error: 
-            print(error," , Moving on with the process")   
+        dir_path = join(curr_wk_dir, dir_name)
+        print("Creating a new directory in the following path :", dir_path, " -- ")
+
+        try:
+            os.mkdir(dir_path)
+        except OSError as error:
+            print(error, " , Moving on with the process")
 
     def cd_to_loc(self, loc):
         """ Switch your Working Directory """
         loc = f"{loc}"
-        #print("Seting Working dir as :", loc)
-        #print(["cd", loc])
-        
-        chg_dir = join(self.parent_dir , loc)
-        print("Parent Dir : ",self.parent_dir)
-        print("dir to be changed to :", chg_dir )
+        # print("Seting Working dir as :", loc)
+        # print(["cd", loc])
+
+        chg_dir = join(self.parent_dir, loc)
+        print("Parent Dir : ", self.parent_dir)
+        print("dir to be changed to :", chg_dir)
         os.chdir(chg_dir)
         self.curr_dir = f"{str(os.getcwd())}"
-        
+
     def del_loc(self, loc):
         """ See the list of all dir tree"""
         __loc__ = f"{loc}"
-        rm_dir = join(self.parent_dir , __loc__)
-        print("Removing directory :: ",rm_dir)
+        rm_dir = join(self.parent_dir, __loc__)
+        print("Removing directory :: ", rm_dir)
         shutil.rmtree(rm_dir)
-        #self.dest = None
+        # self.dest = None
 
     def clone_to_path(self):
         """ Function to clone the repository to a particular local path :: """
@@ -147,33 +148,33 @@ class GitObject:
         """Create a new branch and commit and push the changes into them """
         print("Function Under dev")
 
-    def mv_to_loc(self, source , dest ):
+    def mv_to_loc(self, source, dest):
         """ Copy the contents of cloned dir to wk """
         dest = f"{self.abs_path(dest)}"
 
         source = f"{self.abs_path(source)}"
         os.chdir(source)
         files = os.listdir()
-        
+
         print("Moving the contents of cloned dir to wk")
         for file in files:
-            __source__ = join(source,file)
-            __dest__ = join(dest,file)
-            print("Moving file :",file)
-            try: 
-                shutil.copy(__source__, __dest__,follow_symlinks = False)
-                print("File copied successfully.") 
-  
-            # If source and destination are same 
-            except shutil.SameFileError: 
-                print("Source and destination represents the same file.") 
-            except PermissionError: 
-                print("Permission denied.") 
-  
-            # For other errors 
-            except: 
+            __source__ = join(source, file)
+            __dest__ = join(dest, file)
+            print("Moving file :", file)
+            try:
+                shutil.copy(__source__, __dest__, follow_symlinks=False)
+                print("File copied successfully.")
+
+            # If source and destination are same
+            except shutil.SameFileError:
+                print("Source and destination represents the same file.")
+            except PermissionError:
+                print("Permission denied.")
+
+            # For other errors
+            except:
                 print("Error occurred while copying file.")
-            #print("Moved to :" , result ,"\n")
+            # print("Moved to :" , result ,"\n")
 
     def sync_to_wk(
         self,
@@ -198,10 +199,8 @@ class GitObject:
         )
         subprocess.run(["rsync", "-aP", __exclude_list__, __source__, __dest__])
 
-    
 
-
-# Commands used in jupyter -- 
+# Commands used in jupyter --
 """
     PROJECT_PATH_NEW = join(PROJECT_PATH,GIT_REPOSITORY)
     !mkdir "{PROJECT_PATH_NEW}"
@@ -213,4 +212,4 @@ class GitObject:
     !rsync -aP --exclude=data/ "{PROJECT_PATH_NEW}"/*  ./
     --exclude={'file1.txt','dir1/*','dir2'}
 """
-# -- 
+# --
